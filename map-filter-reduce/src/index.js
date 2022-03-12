8563bfd607b80bde2b9ce5876fdf6b817465a8e7
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-11 23:08:14 -0600

    functions map/filter/reduce/find/findIndex

---
+++ map-filter-reduce/src/index.js
@@ -0,0 +1,60 @@
+import emojipedia from "./emojipedia";
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+//function double(x) {
+//  return x * 2;
+//}
+//const newNumbers = numbers.map(double);
+//console.log(newNumbers);
+//var newNumbers = [];
+
+//numbers.forEach(function (x) {
+//  newNumbers.push(x * 2);
+//});
+//console.log(newNumbers);
+//const newNumbers = numbers.map(function (x) {
+//return x * 2;
+//});
+//console.log(newNumbers);
+
+//Filter - Create a new array by keeping the items that return true.
+//const newNumbers = numbers.filter(function (num) {
+//return num < 10;
+//});
+//var newNumbers = [];
+//numbers.forEach(function (num) {
+//if (num < 10) {
+//newNumbers.push(num);
+//}
+//});
+//console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+//var newNumber = 0;
+//numbers.forEach(function (num) {
+//newNumber += num;
+//});
+//console.log(newNumber);
+//var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//return accumulator + currentNumber;
+//});
+//console.log(newNumber);
+
+//Find - find the first item that matches from an array.
+//const newNumber = numbers.find(function (num) {
+//return num > 10;
+//});
+//console.log(newNumber);
+//FindIndex - find the index of the first item that matches.
+//const newNumber = numbers.findIndex(function (num) {
+//return num > 10;
+//});
+
+//console.log(newNumber);
+
+const items = emojipedia.map(function (item) {
+  return item.meaning.substring(0, 100);
+});
+
+console.log(items);
