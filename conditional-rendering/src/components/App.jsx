aac356ac4421f81d4821de2cf2669f64c8ceadf5
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-14 21:37:01 -0600

    react conditional rendering with ternary operator & And operartor

---
+++ conditional-rendering/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+const currentTime = new Date(2019, 11, 1, 18).getHours();
+
+function App() {
+  return (
+    <div className="container">
+      {
+        //isLoggedIn === true ? <h1>Hello</h1> : <Login />
+        //currentTime > 12 ? <h1>Why are you still working?</h1> : null
+        currentTime > 12 && <h1>Why are you still working?</h1>
+      }
+    </div>
+  );
+}
+
+export default App;
